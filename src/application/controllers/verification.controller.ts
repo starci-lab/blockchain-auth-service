c31eecc2a6176bf5b6d842574f5670d098f69b6c
starci183 <cuongnvtse160875@gmail.com>
2024-09-01 03:29:35 +0700

    123123

--- src/application/controllers/verification.controller.ts
+++ src/application/controllers/verification.controller.ts
@@ -1,8 +1,9 @@
 import { VerificationControllerService } from "@/services"
 import { VerifyMessageRequestBody, VerifyMessageResponse } from "@/services/controllers/dtos"
 import { Body, Controller, HttpCode, HttpStatus, Logger, Post } from "@nestjs/common"
-import { ApiResponse } from "@nestjs/swagger"
+import { ApiResponse, ApiTags } from "@nestjs/swagger"
 
+@ApiTags("Verification")
 @Controller("api/v1/verification")
 export class VerificationController {
     private readonly logger = new Logger(VerificationController.name)
