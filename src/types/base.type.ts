083deeb6b5759de2c8b0f3b3b81814d71fa65050
starci183 <cuongnvtse160875@gmail.com>
2024-09-03 05:24:26 +0700

    123

--- src/types/base.type.ts
+++ src/types/base.type.ts
@@ -29,6 +29,6 @@ export const chainToPlatform = (chain: Chain): Platform => {
     case Chain.Avalanche:
         return Platform.Evm
     default:
-        throw new ChainNotFoundException (`Unknown chain: ${chain}`)
+        throw new ChainNotFoundException (chain)
     }
 }
